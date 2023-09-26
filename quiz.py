264eee84bee8b10fad3d308992ba296deed32a10
zDjalma <catce.2023211mtds0083@aluno.ifpi.edu.br>
2023-09-25 17:28:26 -0700

    first commit

---
+++ quiz.py
@@ -0,0 +1,212 @@
+def quiz():
+    pontos = 0
+    while pontos < 10:
+        print("<-------------------------------->")
+        print('01) Qual e o nome do protagonista de "Tate no Yūsha no Nariagari"')
+        print("a) Naruto")
+        print("b) Naofumi")
+        print("c) Goku")
+        print("d) Tanjiro")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "b" or responta1 == "B" or responta1 == "Naofumi" or responta1 == "naofumi":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('02) Como se Chama A Fruta Que o "LUFFY" comeu?')
+        print("a) Gomu Gomu no Mi")
+        print("b) Banana")
+        print("c) Bara Bara no Mi")
+        print("d) Tangerina")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "a" or responta1 == "A":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('03) Qual a comida favorita do "Narutin"?')
+        print("a) Carne")
+        print("b) Frutas")
+        print("c) Lámen")
+        print("d) Açucar")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "c" or responta1 == "C":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('04) Meliodas morreu para quem?"')
+        print("a) Ban")
+        print("b) Mael")
+        print("c) Rei Demônio")
+        print("d) Estarossa")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "d" or responta1 == "D":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('05) Qual o nome fo irmão da "Yor"?')
+        print("a) Loid")
+        print("b) Anya")
+        print("c) Yuri")
+        print("d) Junior")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "c" or responta1 == "C":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('06) o que toji é do megumi?')
+        print("a) Irmão")
+        print("b) Pai")
+        print("c) Amigos")
+        print("d) Conhecidos")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "b" or responta1 == "B":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('07) Qual é o objetivo dos irmãos Elric?"')
+        print("a) Matar Todos os Homúncolos?")
+        print("b) Encotrar a pedra filosofal e recupera seus corpos")
+        print("c) Dominar o mundo")
+        print("d) Liberdade")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "b" or responta1 == "B":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('08) Qual é o nome do Titã que destruiu a muralha Maria, abrindo caminho para a invasão dos Titãs?"')
+        print("a) Titã Fêmea")
+        print("b) Titã Blindado")
+        print("c) Titã Colossal")
+        print("d) Titã Mandibula")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "c" or responta1 == "C":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('09) Qual foi o primeiro kpeta quem denji encontra na obra(manga e anime)?')
+        print("a) Demonio da motocerra")
+        print("b) Demonio do tomate")
+        print("c) Demonio dos zombie")
+        print("d) Demonio da espada")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "b" or responta1 == "B":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('10) Quem foi o (os) campeão (ões) do jogo Gun Gale Online?')
+        print("a) Kirito")
+        print("b) Asuna")
+        print("c) Shinon")
+        print("d) Kirito e Shinon")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "d" or responta1 == "D":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('11) Qual é o nome dessas gêmeas onis?')
+        print("a) Rim e Rom")
+        print("b) Rum e Rão")
+        print("c) Ram e Rem")
+        print("d) Rym e Rem")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "c" or responta1 == "C":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('12) Em Konosuba Como se chama a "DEUSA DA AGUA"')
+        print("a) Megumim")
+        print("b) Darkness")
+        print("c) Aqua")
+        print("d) Agua")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "c" or responta1 == "C":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("<-------------------------------->")
+        print('13) Quem primeiramente seria a escolha para herdar o poder de All Might?')
+        print("a) Izuku Midorya")
+        print("b) Bakugou Katsuki")
+        print("c) Shoto Todoroki")
+        print("d) Mirio Togata")
+        print("Resposta: ")
+        responta1 = input().strip()
+        if responta1 == "d" or responta1 == "D":
+            print("Certa Resposta")
+            pontos = pontos + 1
+            print("Sua Pontoação é:", pontos)
+        else:
+            print("Resposta Errada")
+            print("Sua Pontuação é:", pontos)
+        print("-------WIN-------")
+        print("Sua Pontução:", pontos)
+        print("voce conhece bem dos animes")
+        
+        
+
+def main():
+    print("QUIZ DE ANIMES")
+    iniciar = input("Precione enter para começar")
+    if True:
+        quiz()
+
+if __name__ == "__main__":
+    main()
